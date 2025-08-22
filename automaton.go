dc1f8f1644c349fcd535ccb0919f87112f753cb5
Amit Prajapati <35319368+amitprajapati027@users.noreply.github.com>
2025-08-22 18:44:45 -0300

    Exposing internal type

--- automaton.go
+++
@@ -1,6 +0,0 @@
-package finiteautomation
-
-import "github.com/amitprajapati027/finite-automation/internal/automaton"
-
-// FiniteAutomation describes a finite automation.
-type FiniteAutomation = automaton.FiniteAutomation

d5254db2976c4b8fd0860c2dd80b8c81c600806f
Amit Prajapati <35319368+amitprajapati027@users.noreply.github.com>
2025-08-22 18:56:13 -0300

    Exposing internal struct

---
+++ finiteautomation.go
@@ -0,0 +1,8 @@
+package finiteautomation
+
+import (
+	"github.com/amitprajapati027/finite-automation/internal/automaton"
+)
+
+// FiniteAutomation describes a finite automation.
+type FiniteAutomation = automaton.FiniteAutomation
